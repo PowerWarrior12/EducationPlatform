bbfd649852f56ef224644822af4469d1f7103765
PowerWarrior12 <businessmail1710@mail.ru>
2023-02-27 08:47:55 +0400

    nemnogo

--- educationtools/src/main/java/com/example/educationtools/logic/CalculationBlock.kt
+++ educationtools/src/main/java/com/example/educationtools/logic/CalculationBlock.kt
@@ -1,11 +1,18 @@
 package com.example.educationtools.logic
 
 class CalculationBlock: LogicBlock() {
+    var nextBlock: LogicBlock? = null
+    var function: Function? = null
+    var changeableVar: String? = null
     override fun start(inputVariables: List<Variable>) {
-        TODO("Not yet implemented")
+        inputVariables.forEach { variable ->
+            variables[variable.name] = variable
+        }
     }
 
     override fun work() {
-
+        changeableVar?.let { changeableVar ->
+            function
+        }
     }
 }
